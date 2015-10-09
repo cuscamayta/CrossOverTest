c66b4acd5231ff63f9251a3ac9ee6661cbfab691
cuscamayta <cuscamayta@gmail.com>
2015-10-09 11:56:32 -0400

    clean routes

--- CrossOverTest.UI/angularjs/app/app.js
+++ CrossOverTest.UI/angularjs/app/app.js
@@ -4,10 +4,7 @@ var app = angular.module('crossOverApp', []);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-     .when('/', { templateUrl: '/angularjs/app/partials/task.html', controller: 'taskController' })
-     .when('/products', { templateUrl: '/Scripts/app/templates/listProduct.html', controller: 'listProductController' })
-     .when('/detail', { templateUrl: '/Scripts/app/templates/detail.html', controller: 'detailController' })
-     .when('/newProduct', { templateUrl: '/Scripts/app/templates/product.html', controller: 'productController' })
+     .when('/', { templateUrl: '/angularjs/app/partials/task.html', controller: 'taskController' })    
      .otherwise({ rediretTo: '/products' });
 }]);
 
